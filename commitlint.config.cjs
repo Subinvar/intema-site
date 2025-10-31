7345f3cc84e451125213f8c6a3e454c1c105305c
Subinvar <sibinvariants@gmail.com>
2025-10-31 15:18:04 +0300

    Обновление

--- commitlint.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    // Allow commit subjects written in Russian (or any language) to retain
-    // their natural capitalization instead of forcing lower-case English.
-    'subject-case': [0],
-  },
-}
